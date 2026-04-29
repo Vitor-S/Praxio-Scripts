fd23ba3f2215d2e820ec4f6cd73654ac684a98ee
Vitor Silva <vitorsilvaoli2001@gmail.com>
2026-04-29 20:12:55 -0300

    first commit

---
+++ sla/sla.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Script SLA
+// @namespace
+// @version      1.0.4
+// @description  Utilitários para o portal do cliente Praxio com múltiplos atalhos
+// @author       Vitor
+// @match        https://portaldocliente.praxio.com.br/Ticket*
+// @require      file:///C:/Users/Vitor/Projetos/UserScript/portalService.js
+// @require      file:///C:/Users/Vitor/Projetos/UserScript/shortcuts.js
+// @run-at       document-idle
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(async function () {
+    'use strict'
+})();
