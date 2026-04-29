9f96f4e5296a2ba6b3bc72b2c4d904b06ba95ec6
Vitor Silva <vitorsilvaoli2001@gmail.com>
2026-04-29 20:26:01 -0300

    alterando arquivo principal

--- sla/sla.user.js
+++ sla/sla.user.js
@@ -1,17 +1,4 @@
 // ==UserScript==
-// @name         Script SLA
-// @namespace
-// @version      1.0.4
-// @description  Utilitários para o portal do cliente Praxio com múltiplos atalhos
-// @author       Vitor
-// @match        https://portaldocliente.praxio.com.br/Ticket*
-// @require      file:///C:/Users/Vitor/Projetos/UserScript/portalService.js
-// @require      file:///C:/Users/Vitor/Projetos/UserScript/shortcuts.js
-// @run-at       document-idle
-// @grant        GM_addStyle
-// ==/UserScript==
-
-// ==UserScript==
 // @name         Vitor Praxio Scripts
 // @namespace    https://portaldocliente.praxio.com.br
 // @version      1.0
