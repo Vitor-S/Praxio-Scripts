958142f838d35cde21dccb7462c5bbe66fd497fc
Vitor Silva <vitorsilvaoli2001@gmail.com>
2026-04-29 20:33:57 -0300

    teste trocando versão 1.2

--- sla/sla.user.js
+++ sla/sla.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Vitor Praxio Scripts
 // @namespace    https://portaldocliente.praxio.com.br
-// @version      1.0
+// @version      1.2
 // @description  Script para o portal do cliente praxio
 // @author       Vitor
 // @match        https://portaldocliente.praxio.com.br/Ticket*
-// @require      https://github.com/Vitor-S/Praxio-Scripts/raw/refs/heads/main/sla/appendElements.js
-// @updateURL    https://github.com/Vitor-S/Praxio-Scripts/raw/refs/heads/main/sla/sla.user.js
-// @downloadURL  https://github.com/Vitor-S/Praxio-Scripts/raw/refs/heads/main/sla/sla.user.js
+// @require      https://raw.githubusercontent.com/Vitor-S/Praxio-Scripts/main/sla/appendElements.js
+// @updateURL    https://raw.githubusercontent.com/Vitor-S/Praxio-Scripts/main/sla/sla.user.js
+// @downloadURL  https://raw.githubusercontent.com/Vitor-S/Praxio-Scripts/main/sla/sla.user.js
 // @run-at       document-idle
 // @grant        GM_addStyle
 // ==/UserScript==
