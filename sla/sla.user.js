472299dc9397201ef8c2a6e560b572fdd96a8d35
Vitor Silva <vitorsilvaoli2001@gmail.com>
2026-04-29 20:25:23 -0300

    alterando arquivo principal

--- sla/sla.user.js
+++ sla/sla.user.js
@@ -18,8 +18,11 @@
 // @description  Script para o portal do cliente praxio
 // @author       Vitor
 // @match        https://portaldocliente.praxio.com.br/Ticket*
+// @require      https://github.com/Vitor-S/Praxio-Scripts/raw/refs/heads/main/sla/appendElements.js
 // @updateURL    https://github.com/Vitor-S/Praxio-Scripts/raw/refs/heads/main/sla/sla.user.js
 // @downloadURL  https://github.com/Vitor-S/Praxio-Scripts/raw/refs/heads/main/sla/sla.user.js
+// @run-at       document-idle
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (async function () {
