30a139283d62135dd8994873407ac4c549146b74
Vitor Silva <vitorsilvaoli2001@gmail.com>
2026-05-01 04:39:39 -0300

    adicionando função de buscar sla

--- sla/functions.js
+++ sla/functions.js
@@ -1,4 +1,4 @@
-function invokeToast(message, icon="warning", time=3500){
+function invokeToast(message, icon = "warning", time = 3500, infinite = false, revoke = false) {
     const toast = document.querySelector("#toast")
     const toastIcon = document.querySelector("#toast-icon")
     const toastMessage = document.querySelector("#toast-message")
@@ -7,7 +7,14 @@ function invokeToast(message, icon="warning", time=3500){
     toastIcon.classList = icon == "check" ? "fa fa-check" : icon == "error" ? "fa fa-times" : "fa fa-exclamation-triangle"
     toastMessage.innerText = message.charAt(0).toUpperCase() + message.slice(1)
 
-    toast.classList.add("active-toast")
+    if (revoke) {
+        toast.classList.remove("active-toast")
+    }
+    else {
+        toast.classList.add("active-toast")
+    }
+
+    if (infinite) return
 
     setTimeout(() => {
         toast.classList.remove("active-toast")
